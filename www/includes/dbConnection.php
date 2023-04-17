22ce5c275a4e7b1a85ba9e6d9a229103341a0282
anastasia-dorfman <anastasia.dorfman1@gmail.com>
2023-04-16 21:22:59 -0300

    initial files added

---
+++ www/includes/dbConnection.php
@@ -0,0 +1,12 @@
+<?php
+        $hostname = "localhost";
+        $username = "dev";
+        $password = "dev1234";
+        $dbName = "anastasia_dorfman_portfolio";
+        
+        $con = new mysqli($hostname, $username, $password, $dbName);
+        if ($con->connect_error) {
+            die("Connection failed" . $conn->connect_error);
+        }
+        
+?>
